f0241851ba436147065ea37c841b46255c909440
Armin0078 <molimol2689@gmail.com>
2024-07-03 21:03:21 +0330

    Models Added.

---
+++ MyTask/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyTask.Models
+{
+	public class Product
+	{
+		[Required, MaxLength(25), MinLength(3)]
+        public string? Name { get; set; }
+
+		[Required]
+		public DateTime ProduceDate { get; set; }
+
+		[Required, MaxLength(11), Phone]
+		public string? ManufacturePhone { get; set; }
+
+		[Required, EmailAddress, MaxLength(30)]
+		public string? ManufactureEmail { get; set; }
+
+		[Required]
+		public bool IsAvailable { get; set; }
+    }
+}
---
+++ MyTask/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyTask.Models
+{
+	public class User
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required, MaxLength(25)]
+		public string? UserName { get; set; }
+	}
+}
